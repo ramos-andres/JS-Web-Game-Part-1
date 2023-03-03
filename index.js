4f3aa3078a60001f8b401333b77c3c1ea4f8baff
Andres Ramos <jedi.politico@gmail.com>
2023-03-02 18:13:31 -0800

    Updated Code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
     object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
+    object.style.left = `${left}px`
+    object.style.bottom = `${bottom}px`
     document.body.append(object)
     return object
 }
